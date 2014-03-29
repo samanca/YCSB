6c6442f064c3dcfe121d4c9d8d46754f8478a391
Amirsaman <amirsaman@saman.ucsd.edu>
2014-03-28 22:32:06 -0700

    support for single node

--- run.sh
+++ run.sh
@@ -4,14 +4,21 @@ if [ ! -z $1 ]; then
 else
     WL="workloads/insert_only"
 fi
+
 if [ ! -z $2 ]; then
     DB=$2
 else
     DB="test"
 fi
-#./bin/ycsb load mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>/dev/null
-#./bin/ycsb load mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB"
-./bin/ycsb load mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>err.txt 1>out.txt
-#./bin/ycsb run mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>/dev/null
-#./bin/ycsb run mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB"
-./bin/ycsb run mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>>err.txt 1>>out.txt
+
+if [ ! -z $3 ]; then
+    ./bin/ycsb load mongodb -P "$WL" -p mongodb.url=localhost:27017 -p mongodb.database="$DB" 2>err.txt 1>out.txt
+    ./bin/ycsb run mongodb -P "$WL" -p mongodb.url=localhost:27017 -p mongodb.database="$DB" 2>>err.txt 1>>out.txt
+else
+    #./bin/ycsb load mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>/dev/null
+    #./bin/ycsb load mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB"
+    ./bin/ycsb load mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>err.txt 1>out.txt
+    #./bin/ycsb run mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>/dev/null
+    #./bin/ycsb run mongodb -s -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB"
+    ./bin/ycsb run mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>>err.txt 1>>out.txt
+fi
