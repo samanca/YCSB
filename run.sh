f65b3937882d76f7105420fe0635d8bff52b7b68
Amirsaman <amirsaman@saman.ucsd.edu>
2014-03-30 17:23:08 -0700

    bug fix: extra {} in DB name

--- run.sh
+++ run.sh
@@ -32,9 +32,9 @@ else
     ./bin/ycsb run mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="$DB" 2>>err.txt 1>>histogram_run.txt
     echo "H-Run complete"
     sleep 5s
-    ./bin/ycsb load mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="{$DB}_2" -p measurementtype=timeseries -p timeseries.granularity=5 2>>err.txt 1>timeseries_load.txt
+    ./bin/ycsb load mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="${DB}_2" -p measurementtype=timeseries -p timeseries.granularity=5 2>>err.txt 1>timeseries_load.txt
     echo "T-Load complete"
     sleep 3s
-    ./bin/ycsb run mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="{$DB}_2" -p measurementtype=timeseries -p timeseries.granularity=5  2>>err.txt 1>>timeseries_run.txt
+    ./bin/ycsb run mongodb -P "$WL" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="${DB}_2" -p measurementtype=timeseries -p timeseries.granularity=5  2>>err.txt 1>>timeseries_run.txt
     echo "T-Run complete"
 fi
