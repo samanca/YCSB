c3437c1f50e2a6bac0820ab3c3db6bd3e9cc0d00
Amirsaman <amirsaman@saman.ucsd.edu>
2014-04-01 20:44:49 -0700

    drop db after test

--- run.sh
+++ run.sh
@@ -50,3 +50,5 @@ else
     ./bin/ycsb run mongodb -P "$WL" -threads "$THREADS" -p mongodb.url=localhost:27018,localhost:27019 -p mongodb.database="${DB}_2" -p mongodb.writeConcern="$WrConc" -p measurementtype=timeseries -p timeseries.granularity=5  2>>err.txt 1>>timeseries_run.txt
     echo "T-Run complete"
 fi
+./../mongo/mongo "$DB" --eval "db.dropDatabase()"
+./../mongo/mongo "${DB}_2" --eval "db.dropDatabase()"
