f90fbccb0009f4ce82eaba39b4bc934d6bdb8da1
Amirsaman <amirsaman@saman.ucsd.edu>
2014-04-03 11:54:10 -0700

    script to safe-kill mongod instance

---
+++ kill_mongod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -z $1 ]; then
+    PRIMARY_PORT=$1
+else
+    PRIMARY_PORT=27019
+fi
+
+if [ ! -z $2 ] && [ "$2" == "safe" ]; then
+    SIGNAL=1
+else
+    SIGNAL=9
+fi
+
+PRIMARY_PID=`netstat -tlpn "$PRIMARY_PORT" | grep :"$PRIMARY_PORT" | awk '{ print $7 }' | grep /mongod | awk -F\/ '{ print $1 }'`
+kill -"$SIGNAL" $PRIMARY_PID
